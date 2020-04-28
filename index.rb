bdb339129b1cb4c9206719d6489b8494211d1fca
yukie0525 <siduku_dx@yahoo.co.jp>
2020-04-28 17:34:18 +0900

    Update index.rb

--- index.rb
+++ index.rb
@@ -251,3 +251,27 @@
 # end
 # sayHi("taguchi")
 # sayHi
+
+# def sayHi(name = "tom")
+#     return "hi! #{name}"
+# end
+# p sayHi
+
+# クラス
+# class User
+
+#     def initialize(name)
+#         @name = name
+#     end
+
+#     def sayHi
+#         puts "hi! i am #{@name}"
+#     end
+
+# end
+
+# tom = User.new("tom")
+# tom.sayHi
+
+# bob = User.new("bob")
+# bob.sayHi
